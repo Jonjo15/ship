a99311bf65811c6f1c5f72087b4904c5861ccda3
IVAN <ivanbatur5g@gmail.com>
2020-10-22 17:36:22 +0200

    Did the ship part

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,5 +1,12 @@
-function example() {
-    return true
+const Ship = (length) => {
+    const hitArray = new Array(length).fill(false)
+    const getLength = () => length
+    const hit = (pos) => {
+        hitArray[pos] = true;
+        return hitArray
+    }
+    const isSunk = () => hitArray.every(val => val)
+    return {getLength, hit, isSunk}
 }
 
-export default example
+export default Ship
--- src/factories/ship.test.js
+++ src/factories/ship.test.js
@@ -1,5 +1,23 @@
-import example from "./ship"
+import Ship from "./ship"
 
-test("Test", () => {
-    expect(example()).toBe(true)
+test("setting ships length works,", () => {
+    expect(Ship(4).getLength()).toBe(4)
+})
+test("hitting the ship works", () => {
+    let ship = Ship(4)
+    expect(ship.hit(0)).toStrictEqual([true,false,false,false])
+})
+test("hitting the ship works(2)", () => {
+    let ship = Ship(3)
+    ship.hit(0)
+    expect(ship.hit(1)).toStrictEqual([true,true,false])
+})
+test("isSunk should return false if ship not hit", () => {
+    expect(Ship(4).isSunk()).toBe(false)
+})
+test("isSunk should return true if all ships positions hit",() => {
+    let ship = Ship(2)
+    ship.hit(0)
+    ship.hit(1)
+    expect(ship.isSunk()).toBe(true)
 })
