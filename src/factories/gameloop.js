1c649af3280f324b7a48045a4b20290648528782
IVAN <ivanbatur5g@gmail.com>
2020-10-25 14:41:53 +0100

    Started crafting ui on index.html

--- src/factories/gameloop.js
+++ src/factories/gameloop.js
@@ -4,4 +4,27 @@ const game = (() => {
     let player1 = Player();
     let player2 = Player(true);
     let playersArray = [player1, player2]
+    let winner;
+    const checkForWin = () => {
+        if (player1.gameboard.allShipsSunk()) {
+            winner = "CPU"
+            gameOver = true;
+        }
+        else if (player2.gameboard.allShipsSunk()) {
+            winner = "YOU"
+            gameOver = true;
+        }
+    }
+
+    const playTurn = () => {
+
+    }
+
+    const startGame = () => {
+
+    }
+
+    const reset = () => {
+
+    }
 })()
